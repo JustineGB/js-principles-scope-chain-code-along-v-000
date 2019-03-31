1570626801a193d3e813a9370fce6f71592b0e29
Justine B <justinegayton@gmail.com>
2019-03-31 19:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,6 @@ function changeLeastFavoriteCustomer() {
 
 function attemptTwoFavoriteCustomers() {
   let favoriteCustomer = "Sally";
+    let favoriteCustomer = "Mary";
+
 }   
-  let favoriteCustomer = "Mary";
