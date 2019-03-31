2af95431fb384c4b8611b84002de5b59cadcd890
Justine B <justinegayton@gmail.com>
2019-03-31 19:16:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function setBestCustomer() {
   bestCustomer = "not bob";
 }
 
-// function overWriteBestCustomer() {
-//   bestCustomer = "maybe bob";
-// }
+function overWriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
 
 // function changeLeastFavoriteCustomer() {
 //   leastFavoriteCustomer = "bob";
